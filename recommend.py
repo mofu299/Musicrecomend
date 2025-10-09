1d35afe8ea88aef07712f090b92beaff2a6a04f7
mofu299 <yuricchu55@gmail.com>
2025-10-09 05:30:37 +0000

    BPMを実装

--- recommend.py
+++ recommend.py
@@ -5,7 +5,7 @@ from gensim.models import KeyedVectors
 
 # ── 読み込み ───────────────────────────
 #bpmだけまだ前段階のデータ
-bpm = np.load("data/combined_vector.npy", allow_pickle=True) 
+bpm = np.load("data/bpm_vector.npy", allow_pickle=True) 
 title = np.load("data/name.npy", allow_pickle=True)
 
 # ファイル名と特徴量の二次元配列
@@ -44,10 +44,9 @@ st.write("楽曲レコメンドアプリ")
 
 # ここを修正: 検索リストを「曲のタイトル」に変更
 st.markdown("#テーマに対して似ている曲を表示する")
-
+results = []
 if selected_feature:
     st.markdown(f"### {selected_feature} に関連する曲")
-    results = []
     best_score = -1  # 初期値として最小スコアを設定
     best_track = ""
     
@@ -60,33 +59,51 @@ if selected_feature:
     
     st.dataframe(pd.DataFrame(results))
 
+#resultsのスコアとBPMを組み合わせて、運動量に応じた曲を推薦
+stretch = np.array([1, 60])
+work = np.array([1, 120])
+jog = np.array([1, 145])
+run = np.array([1, 170])
+
 # 「BPM」に変更
 run_options = {
-    "ストレッチ" : 60,
-    "ウォーキング": 120,
-    "ジョギング" : 145,
-    "ランニング": 170,
+    "ストレッチ" : stretch,
+    "ウォーキング": work,
+    "ジョギング" : jog,
+    "ランニング": run
 }
 # 運動量の選択
-selected_run = st.selectbox("特徴量を選んでください", list(run_options.keys()))
+selected_run = st.selectbox("特徴量を選んでください", run_options.keys())
 # 条件に応じてスコアを変更
 run_scores = run_options[selected_run]
 st.markdown("#テーマに対して似ている曲を表示する")
 
+#resultのスコアを取り出す
+score_results = np.array([results[i]["score"] for i in range(len(results))])
+#bpmとresultsのタイトルが一致するもののBPMを取り出す
+keep = np.array([bpm[i, 1] for i in range(len(bpm)) if bpm[i, 0] in [results[j]["title"] for j in range(len(results))]])
+
+recommend_score = np.array([keep, score_results]).T
+keep_titles = list(results[i]["title"] for i in range(len(results)))
+
+kv = KeyedVectors(vector_size=len(run_scores))  # 今回は BPM のみなので 1次元
+kv.add_vectors(list(keep_titles), recommend_score)  # numpy 配列に変換して追加
+kv.fill_norms()
+
+result2 = []
 if selected_run:
     st.markdown(f"### {selected_run} に関連する曲")
-    results = []
     best_score = -1  # 初期値として最小スコアを設定
     best_track = ""
     
     # 選択された特徴量を基に、最も類似した曲を探す
-    for recommend_track, score in kv.most_similar(run_scores, topn=30):
-        results.append({"title": recommend_track, "score": score})
+    for recommend_track, score in kv.similar_by_vector(run_scores, topn=30):
+        result2.append({"title": recommend_track, "score": score})
         if score > best_score:
             best_score = score
             best_track = recommend_track
     
-    st.dataframe(pd.DataFrame(results))
+    st.dataframe(pd.DataFrame(result2))
     url = f"https://www.google.com/search?q={best_track}+楽曲"
     st.markdown(f"[ {best_track}をGoogleで検索]({url})")
 
